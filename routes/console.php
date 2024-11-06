6bcba34b976cded626f2453af5fa6b33938ca422
Kevin Chandra <kacol.bot@gmail.com>
2024-11-06 15:23:53 +0700

    fix opcache force command

--- routes/console.php
+++ routes/console.php
@@ -7,8 +7,10 @@ Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote')->hourly();
 
-Artisan::command('deploy', function (){
+Artisan::command('deploy', function () {
     $this->call('optimize:clear');
     $this->call('optimize');
-    $this->call('opcache:compile --force');
+    $this->call('opcache:compile', [
+        '--force' => true,
+    ]);
 });
