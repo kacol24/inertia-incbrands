79f74578c60991085a85ea4d79b4933e30fb1e65
Kevin Chandra <kacol.bot@gmail.com>
2024-11-06 15:21:29 +0700

    shorthand deploy command

--- routes/console.php
+++ routes/console.php
@@ -6,3 +6,11 @@ use Illuminate\Support\Facades\Artisan;
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote')->hourly();
+
+Artisan::command('deploy', function (){
+    $this->call('optimize:clear');
+    $this->call('optimize');
+    $this->call('opcache:compile', [
+        '--force'
+    ]);
+});
